1506ed3483e77b0f18edc6098d63b7bcde5cad8e
Ch3my <35230589+Ch3my@users.noreply.github.com>
2021-06-08 13:34:54 -0400

    W8

--- src/app/contacts/contact-edit/contact-edit.component.ts
+++ src/app/contacts/contact-edit/contact-edit.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { ActivatedRoute, Params, Router } from '@angular/router';
+import { Contact } from '../contact.model';
+import { ContactService } from '../contact.service';
 
 @Component({
   selector: 'cms-contact-edit',
@@ -6,10 +10,96 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./contact-edit.component.css']
 })
 export class ContactEditComponent implements OnInit {
+  originalContact: Contact;
+  contact: Contact;
+  groupContacts: Contact[] = [];
+  editMode: boolean = false;
+  id: string;
 
-  constructor() { }
+  constructor(private contactService: ContactService, private router: Router, private route: ActivatedRoute) { }
 
   ngOnInit(): void {
+    this.route.params.subscribe(
+      (params: Params) => {
+        const id = params['id'];
+
+        if (!id) {
+          this.editMode = false;
+          return;
+        }
+
+        this.originalContact = this.contactService.getContact(id);
+
+        if (!this.originalContact) {
+          return;
+        }
+
+        this.editMode = true;
+        this.contact = JSON.parse(JSON.stringify(this.originalContact));
+        if (this.contact.group !== null && this.contact.group !== undefined) {
+          this.groupContacts = JSON.parse(JSON.stringify(this.contact.group));
+        }
+      })
   }
 
+  onCancel() {
+    this.router.navigate(['/contacts'], { relativeTo: this.route });
+  }
+
+  onSubmit(form: NgForm) {
+    // Read the form
+    const value = form.value
+    const newContact = new Contact(this.originalContact?.id, value.name, value.email, value.phone, value.imageUrl, this.originalContact?.group)
+
+    // Edit if edit or new by default
+    if (this.editMode == true) {
+      this.contactService.updateContact(this.originalContact, newContact)
+    } else {
+      this.contactService.addContact(newContact);
+    }
+    this.router.navigate(['/contacts'], { relativeTo: this.route });
+  }
+
+
+  addToGroup($event: any) {
+    const selectedContact: Contact = $event.dragData;
+    const invalidGroupContact = this.isInvalidContact(selectedContact);
+    if (invalidGroupContact) {
+      return;
+    }
+    this.groupContacts.push(selectedContact);
+  }
+
+
+  onRemoveItem(index: number) {
+    if (index < 0 || index >= this.groupContacts.length) {
+      return;
+    }
+    this.groupContacts.splice(index, 1);
+  }
+
+  isInvalidContact(newContact: Contact) {
+		if (!newContact) {
+			return true;
+		}
+
+    // Check if editing. Otherwise this.contact is undefined
+		// If the contact being draged is the same of the contact we are at
+    if(this.editMode){
+      if (newContact.id === this.contact.id) {
+        return true;
+      }
+    }
+
+		for (let i = 0; i < this.groupContacts.length; i++) {
+			// Check if the contact being dragged already exists
+			if (newContact.id === this.groupContacts[i].id) {
+				//is INVALID
+				return true;
+			}
+		}
+
+		//if passes all tests, we are OK
+		return false;
+	}
 }
--- src/app/documents/document-edit/document-edit.component.ts
+++ src/app/documents/document-edit/document-edit.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { ActivatedRoute, Params, Router } from '@angular/router';
+import { Document } from '../document.model';
+import { DocumentService } from '../document.service';
 
 @Component({
   selector: 'cms-document-edit',
@@ -6,10 +10,55 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./document-edit.component.css']
 })
 export class DocumentEditComponent implements OnInit {
+  originalDocument: Document
+  document: Document 
+  editMode: boolean = false
 
-  constructor() { }
+  constructor(private documentService: DocumentService, private router: Router, private route: ActivatedRoute) { }
 
   ngOnInit(): void {
+    this.route.params.subscribe(
+      (params: Params) => {
+        //get id from parameters
+        const id = params['id'];
+
+        //if id doesnt exist on parameters..
+        if (!id) {
+          //set edit mode to false and exit..
+          this.editMode = false;
+          return;
+        }
+        //if it exists on params..
+        //get and store the doc with that id in original document prop
+        this.originalDocument = this.documentService.getDocument(id);
+
+        //if not document is found with that id...
+        if (!this.originalDocument) {
+          //exit function
+          return;
+        }
+
+        this.editMode = true;
+        // Clone original Doc
+        this.document = JSON.parse(JSON.stringify(this.originalDocument));
+      })
   }
 
+  onCancel() {
+    this.router.navigate(['/documents'], { relativeTo: this.route });
+  }
+
+  onSubmit(form: NgForm) {
+    // Read the form
+    const value = form.value
+    const newDocument = new Document(this.originalDocument?.id, value.name, value.description, value.url, this.originalDocument?.children)
+
+    // Edit if edit or new by default
+    if (this.editMode == true) {
+      this.documentService.updateDocument(this.originalDocument, newDocument)
+    } else {
+      this.documentService.addDocument(newDocument);
+    }
+    this.router.navigate(['/documents'], { relativeTo: this.route });
+  }
 }
